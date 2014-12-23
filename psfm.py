f52f15111719d7ec7366d457668cfc46fb471efd
Graham Gower <graham.gower@gmail.com>
2014-12-23 22:20:31 +1030

    Add some copyright lines

--- psfm.py
+++ psfm.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python3
 
 # Play Some Fucking Music, using XMMS2.
+#
+# Copyright (c) 2014 Graham Gower <graham.gower@gmail.com>
+#
+# Permission to use, copy, modify, and distribute this software for any
+# purpose with or without fee is hereby granted, provided that the above
+# copyright notice and this permission notice appear in all copies.
+#
+# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 import os
 import sys
 import urllib.parse
-import math
 from collections import OrderedDict
 
 from tkinter import *
